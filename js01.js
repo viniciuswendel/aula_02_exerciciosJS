5e87991e68cba347594efcbdda658f57d86ac83b
vinicius.wendel <vinicius.wendel.ti@gmail.com>
2024-10-07 12:42:14 -0300

    exerciciosAula02

---
+++ js01.js
@@ -0,0 +1,13 @@
+const cpf = "123.456.789-09"
+var nome = "Vinícius"
+
+function sobreNome() {
+    let sobreNome = "Wendel"
+    
+    return sobreNome
+}
+
+console.log(nome)
+console.log(sobreNome())
+console.log(cpf)
+console.log(`O nome é: ${nome}.O sobrenome é: ${sobreNome()}. O CPF é: ${cpf}`)
